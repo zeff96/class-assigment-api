21762cedecd7b09856a81d1160bfa37c4e3b9a37
zeff96 <zeffo1975@gmail.com>
2026-01-17 15:18:07 +0300

    Add course controller for adding new course

---
+++ src/controllers/course.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+
+export async function createCourse(req: Request, res: Response) {
+  try {
+    const db = req.app.locals.db;
+
+    const course = {
+      ...req.body,
+      createdAt: new Date(),
+    };
+
+    const result = await db.collection("courses").insertOne(course);
+    res.status(201).json({ ...course, _id: result.insertedId.toHexString() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+}
