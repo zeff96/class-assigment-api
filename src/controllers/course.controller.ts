ec0ff5adb3713399b328513869a8ccfb71e3699e
zeff96 <zeffo1975@gmail.com>
2026-01-18 17:57:16 +0300

    Add controller for fetching all courses and a single course by name

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express";
+import { Course } from "../models/course.model.js";
 
 export async function createCourse(req: Request, res: Response) {
   try {
@@ -14,5 +15,32 @@ export async function createCourse(req: Request, res: Response) {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error", error });
+  }
+}
+
+async function fetchAllCourses(req: Request, res: Response) {
+  try {
+    const db = req.app.locals.db;
+    const coursesLists = (await Course.fetchAllCourses(db)) ?? [];
+    res.status(200).json(coursesLists);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+}
+
+async function findCourseByName(req: Request, res: Response) {
+  try {
+    const db = req.app.locals.db;
+    const name = req.body;
+    const course = await Course.findByName(name, db);
+    if (course === null) {
+      res.status(404).json({ message: `Course with name ${name} not found!` });
+      return;
+    }
+    res.status(200).json(course);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server error", error });
   }
 }
