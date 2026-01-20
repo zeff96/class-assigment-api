ff048f39893ad387e47ffd1d2ca91e758b2ea62d
zeff96 <zeffo1975@gmail.com>
2026-01-20 18:14:58 +0300

    Add validator for both body, params and query strings

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,14 +1,25 @@
 import type { Request, Response, NextFunction } from "express";
 import { ZodType } from "zod";
 
-export function middlewareValidator<T>(schema: ZodType<T>) {
-  return function validator(
-    req: Request<{}, {}, T>,
-    _res: Response,
-    next: NextFunction,
-  ) {
+export type Schemas = {
+  body?: ZodType<any>;
+  params?: ZodType<Record<string, string>>;
+  query?: ZodType<Record<string, string>>;
+};
+
+export function middlewareValidator(schemas: Schemas) {
+  return function validator(req: Request, _res: Response, next: NextFunction) {
     try {
-      req.body = schema.parse(req.body);
+      if (schemas.body) {
+        req.body = schemas.body.parse(req.body);
+      }
+      if (schemas.params) {
+        req.params = schemas.params.parse(req.params);
+      }
+      if (schemas.query) {
+        req.query = schemas.query.parse(req.query);
+      }
+
       next();
     } catch (error) {
       next(error);
