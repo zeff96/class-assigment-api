09624c215604ae7f2db5d85ddc09800a718d11a7
zeff96 <zeffo1975@gmail.com>
2026-01-20 16:50:14 +0300

    Add controller for updating a course

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -4,18 +4,28 @@ import { CourseModel } from "../models/course.model.js";
 export class CourseController {
   constructor(private courseModel: CourseModel) {}
 
-  createCourse = async(req: Request, res: Response) =>{
+  createCourse = async (req: Request, res: Response) => {
     const result = await this.courseModel.create(req.body);
     res.status(201).json(result);
-  }
+  };
 
-  getAllCourses = async(_req: Request, res: Response) => {
+  getAllCourses = async (_req: Request, res: Response) => {
     const courses = await this.courseModel.findAll();
     res.status(200).json(courses);
-  }
+  };
 
-  findByName = async(req: Request, res: Response) =>{
-    const course = await this.courseModel.findByName(req.body);
+  findByName = async (req: Request<{name: string}>, res: Response) => {
+    const course = await this.courseModel.findByName(req.body.name);
     res.status(200).json(course);
-  }
+  };
+
+  updateCourse = async (req: Request, res: Response) => {
+    const result = await this.courseModel.update(req.body);
+    res.status(201).json(result);
+  };
+
+  deleteCourse = async (req: Request, res: Response) => {
+    await this.courseModel.deleteCourse(req.body);
+    res.status(204);
+  };
 }
