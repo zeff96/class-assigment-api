6b17b4d62bb8f7acd0e9196605c0a08435ec7c99
zeff96 <zeffo1975@gmail.com>
2026-01-21 22:12:39 +0300

    Add update schema for updating given course

--- src/schemas/course.schemas.ts
+++ src/schemas/course.schemas.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import z from "zod/v4";
 
 export const courseSchema = z.object({
   name: z.string().min(2, "Must be atleast 2 characters long"),
@@ -6,3 +6,16 @@ export const courseSchema = z.object({
   semester: z.string().min(1, "Must be atleast 1 characters long"),
 });
 
+const updateCourseSchema = courseSchema
+  .partial()
+  .refine((data) => Object.keys(data).length > 0, {
+    message: "Atleast one field must be provided",
+  });
+
+const courseIdParams = z.object({
+  id: z.string().min(1),
+});
+
+export type CreateCourseInput = z.infer<typeof courseSchema>;
+
+export type UpdateCourseSchema = z.infer<typeof updateCourseSchema>;
