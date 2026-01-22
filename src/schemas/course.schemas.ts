037fcea6fb429c044ac421b0b69fb48e07127a4a
zeff96 <zeffo1975@gmail.com>
2026-01-22 13:36:34 +0300

    Add new schemas for validation

--- src/schemas/course.schemas.ts
+++ src/schemas/course.schemas.ts
@@ -1,4 +1,7 @@
+import { ObjectId } from "mongodb";
 import z from "zod/v4";
+
+// schemas
 
 export const courseSchema = z.object({
   name: z.string().min(2, "Must be atleast 2 characters long"),
@@ -12,10 +15,27 @@ const updateCourseSchema = courseSchema
     message: "Atleast one field must be provided",
   });
 
-const courseIdParams = z.object({
+export const courseIdParams = z.object({
   id: z.string().min(1),
 });
 
+export const responseSchema = courseSchema.extend({
+  _id: z
+    .union([z.string(), z.instanceof(ObjectId)])
+    .transform((val) => val.toString()),
+  createdAt: z.date(),
+});
+
+export const listResponseSchema = z.array(responseSchema);
+
+// types
+
 export type CreateCourseInput = z.infer<typeof courseSchema>;
 
-export type UpdateCourseSchema = z.infer<typeof updateCourseSchema>;
+export interface ICourse extends CreateCourseInput {
+  _id: ObjectId;
+  createdAt: Date;
+}
+export type ICourseResponse = z.infer<typeof responseSchema>;
+
+export type UpdateCourseInput = z.infer<typeof updateCourseSchema>;
