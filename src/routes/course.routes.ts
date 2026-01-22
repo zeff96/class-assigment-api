5f1c0de0544f96f6ca8f3324580fb7be3798d85b
zeff96 <zeffo1975@gmail.com>
2026-01-22 13:36:49 +0300

    Update course routes

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -1,27 +1,40 @@
 import { Router } from "express";
-import { middlewareValidator, type Schemas } from "../middleware/validate.js";
-import { courseSchema } from "../schemas/course.schemas.js";
+import { middlewareValidator } from "../middleware/validate.js";
+import { courseIdParams, courseSchema } from "../schemas/course.schemas.js";
 import { CourseController } from "../controllers/course.controller.js";
 import { CourseModel } from "../models/course.model.js";
 import { connectDB } from "../config/db.config.js";
 import { paramSchemaName } from "../schemas/course.params.schema.js";
+import { CourseServices } from "../services/course.service.js";
+import { catchAsync } from "../utils/catchAsync.js";
 
 const db = await connectDB();
 const courseModel = new CourseModel(db);
-const courseController = new CourseController(courseModel);
+const courseService = new CourseServices(courseModel);
+const courseController = new CourseController(courseService);
 
 const router: Router = Router();
 
 router.post(
   "/",
   middlewareValidator({ body: courseSchema }),
-  courseController.createCourse,
+  catchAsync(courseController.createCourse),
 );
-router.get("/", courseController.getAllCourses);
+router.get("/", catchAsync(courseController.getAllCourses));
 router.get(
   "/:name",
   middlewareValidator({ params: paramSchemaName }),
-  courseController.findByName,
+  catchAsync(courseController.findByName),
+);
+router.patch(
+  "/:id",
+  middlewareValidator({ params: courseIdParams }),
+  catchAsync(courseController.updateCourse),
+);
+router.delete(
+  "/:id",
+  middlewareValidator({ params: courseIdParams }),
+  catchAsync(courseController.deleteCourse),
 );
 
 export default router;
