dfa97ae96110a12807a0b3bbc580c7ff4cc2b33e
zeff96 <zeffo1975@gmail.com>
2026-01-23 18:45:07 +0300

    Add code refactor to use a single course router wrapper

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -3,38 +3,37 @@ import { middlewareValidator } from "../middleware/validate.js";
 import { courseIdParams, courseSchema } from "../schemas/course.schemas.js";
 import { CourseController } from "../controllers/course.controller.js";
 import { CourseModel } from "../models/course.model.js";
-import { connectDB } from "../config/db.config.js";
 import { paramSchemaName } from "../schemas/course.params.schema.js";
 import { CourseServices } from "../services/course.service.js";
 import { catchAsync } from "../utils/catchAsync.js";
+import type { Db } from "mongodb";
 
-const db = await connectDB();
-const courseModel = new CourseModel(db);
-const courseService = new CourseServices(courseModel);
-const courseController = new CourseController(courseService);
+export function setupCourseRoutes(db: Db): Router {
+  const router: Router = Router();
+  const courseModel = new CourseModel(db);
+  const courseService = new CourseServices(courseModel);
+  const courseController = new CourseController(courseService);
+  router.post(
+    "/",
+    middlewareValidator({ body: courseSchema }),
+    catchAsync(courseController.createCourse),
+  );
+  router.get("/", catchAsync(courseController.getAllCourses));
+  router.get(
+    "/:name",
+    middlewareValidator({ params: paramSchemaName }),
+    catchAsync(courseController.findByName),
+  );
+  router.patch(
+    "/:id",
+    middlewareValidator({ params: courseIdParams }),
+    catchAsync(courseController.updateCourse),
+  );
+  router.delete(
+    "/:id",
+    middlewareValidator({ params: courseIdParams }),
+    catchAsync(courseController.deleteCourse),
+  );
 
-const router: Router = Router();
-
-router.post(
-  "/",
-  middlewareValidator({ body: courseSchema }),
-  catchAsync(courseController.createCourse),
-);
-router.get("/", catchAsync(courseController.getAllCourses));
-router.get(
-  "/:name",
-  middlewareValidator({ params: paramSchemaName }),
-  catchAsync(courseController.findByName),
-);
-router.patch(
-  "/:id",
-  middlewareValidator({ params: courseIdParams }),
-  catchAsync(courseController.updateCourse),
-);
-router.delete(
-  "/:id",
-  middlewareValidator({ params: courseIdParams }),
-  catchAsync(courseController.deleteCourse),
-);
-
-export default router;
+  return router;
+}
