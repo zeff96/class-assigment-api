26e285bb1455ac08ff8e3426902c56c54883408c
zeff96 <zeffo1975@gmail.com>
2026-01-23 18:40:45 +0300

    Add code refactor to use a single app entry

--- src/app.ts
+++ src/app.ts
@@ -6,36 +6,40 @@ import express, {
 } from "express";
 import cors from "cors";
 import z, { ZodError } from "zod";
-import courseRoutes from "./routes/course.routes.js";
+import { setupCourseRoutes } from "./routes/course.routes.js";
+import type { Db } from "mongodb";
 
-const app: Express = express();
+export function initApp(db: Db): Express {
+  const app: Express = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  const courseRoutes = setupCourseRoutes(db);
 
-app.use("/api/v1/courses", courseRoutes);
+  app.use("/api/v1/courses", courseRoutes);
 
-app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-  console.error(err.stack);
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err.stack);
 
-  if (err instanceof ZodError) {
-    res.status(400).json({
+    if (err instanceof ZodError) {
+      res.status(400).json({
+        status: "error",
+        errors: z.prettifyError(err),
+      });
+      return;
+    }
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
       status: "error",
-      errors: z.prettifyError(err),
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : err.message,
     });
-    return;
-  }
-
-  const statusCode = err.statusCode || 500;
-
-  res.status(statusCode).json({
-    status: "error",
-    message:
-      process.env.NODE_ENV === "production"
-        ? "Internal Server Error"
-        : err.message,
   });
-});
 
-export default app;
+  return app;
+}
